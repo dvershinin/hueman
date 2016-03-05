78ed4382796a36fc681e5ac1407e160a1a1eb283
Danila Vershinin <ciapnz@gmail.com>
2016-03-06 03:28:04 +0400

    The Widget

--- single.php
+++ single.php
@@ -1,39 +1,45 @@
 <?php get_header(); ?>
 
 <section class="content">
-	
+
 	<?php get_template_part('inc/page-title'); ?>
-	
+
+	<?php if ( is_active_sidebar( 'top-title-widget' ) ) : ?>
 	<div class="pad group">
-		
+		<?php dynamic_sidebar( 'top-title-widget' ); ?>
+	</div>
+	<?php endif; ?>
+
+	<div class="pad group">
+
 		<?php while ( have_posts() ): the_post(); ?>
-			<article <?php post_class(); ?>>	
+			<article <?php post_class(); ?>>
 				<div class="post-inner group">
-					
+
 					<h1 class="post-title"><?php the_title(); ?></h1>
 					<p class="post-byline"><?php _e('by','hueman'); ?> <?php the_author_posts_link(); ?> &middot; <?php the_time(get_option('date_format')); ?></p>
-					
+
 					<?php if( get_post_format() ) { get_template_part('inc/post-formats'); } ?>
-					
+
 					<div class="clear"></div>
-					
-					<div class="entry <?php if ( ot_get_option('sharrre') != 'off' ) { echo 'share'; }; ?>">	
+
+					<div class="entry <?php if ( ot_get_option('sharrre') != 'off' ) { echo 'share'; }; ?>">
 						<div class="entry-inner">
 							<?php the_content(); ?>
 							<?php wp_link_pages(array('before'=>'<div class="post-pages">'.__('Pages:','hueman'),'after'=>'</div>')); ?>
 						</div>
 						<?php if ( ot_get_option('sharrre') != 'off' ) { get_template_part('inc/sharrre'); } ?>
-						<div class="clear"></div>				
+						<div class="clear"></div>
 					</div><!--/.entry-->
-					
-				</div><!--/.post-inner-->	
-			</article><!--/.post-->				
+
+				</div><!--/.post-inner-->
+			</article><!--/.post-->
 		<?php endwhile; ?>
-		
+
 		<div class="clear"></div>
-		
+
 		<?php the_tags('<p class="post-tags"><span>'.__('Tags:','hueman').'</span> ','','</p>'); ?>
-		
+
 		<?php if ( ( ot_get_option( 'author-bio' ) != 'off' ) && get_the_author_meta( 'description' ) ): ?>
 			<div class="author-bio">
 				<div class="bio-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
@@ -42,15 +48,15 @@
 				<div class="clear"></div>
 			</div>
 		<?php endif; ?>
-		
+
 		<?php if ( ot_get_option( 'post-nav' ) == 'content') { get_template_part('inc/post-nav'); } ?>
-		
+
 		<?php if ( ot_get_option( 'related-posts' ) != '1' ) { get_template_part('inc/related-posts'); } ?>
-		
+
 		<?php comments_template('/comments.php',true); ?>
-		
+
 	</div><!--/.pad-->
-	
+
 </section><!--/.content-->
 
 <?php get_sidebar(); ?>
