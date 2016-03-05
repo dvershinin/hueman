600a122318e749a2b444ac80014b2c78c3976d4a
Danila Vershinin <ciapnz@gmail.com>
2016-03-06 02:40:58 +0400

    Date Microformat

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('group'); ?>>	
+<article id="post-<?php the_ID(); ?>" <?php post_class('group'); ?>>
 	<div class="post-inner post-hover">
-		
+
 		<div class="post-thumbnail">
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
 				<?php if ( has_post_thumbnail() ): ?>
@@ -16,21 +16,21 @@
 				<a class="post-comments" href="<?php comments_link(); ?>"><span><i class="fa fa-comments-o"></i><?php comments_number( '0', '1', '%' ); ?></span></a>
 			<?php endif; ?>
 		</div><!--/.post-thumbnail-->
-		
+
 		<div class="post-meta group">
 			<p class="post-category"><?php the_category(' / '); ?></p>
-			<p class="post-date"><?php the_time('j M, Y'); ?></p>
+			<abbr class="post-date updated published" title="<?php the_time('Y-m-d\TH:i:s\Z') ?>"><?php the_time('j M, Y'); ?></abbr>
 		</div><!--/.post-meta-->
-		
-		<h2 class="post-title">
+
+		<h2 class="post-title entry-title">
 			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 		</h2><!--/.post-title-->
-		
+
 		<?php if (ot_get_option('excerpt-length') != '0'): ?>
-		<div class="entry excerpt">				
+		<div class="entry excerpt">
 			<?php the_excerpt(); ?>
 		</div><!--/.entry-->
 		<?php endif; ?>
-		
-	</div><!--/.post-inner-->	
-</article><!--/.post-->	
+
+	</div><!--/.post-inner-->
+</article><!--/.post-->
